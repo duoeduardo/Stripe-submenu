49fb4fa1a9d8da0185a8dfb50c87987ab4e90f3a
Eduardo <duoeduardo@gmail.com>
2020-12-08 21:14:32 -0300

    Se agrega heroe

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,21 @@ import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className='hero-info'>
+          <h1>Infraestructura de pagos para Internet</h1>
+          <p>Millones de empresas de todos los tamaños, desde startups hasta grandes empresas, usan el software y las API de Stripe para aceptar pagos, enviar transferencias y gestionar sus actividades comerciales en Internet.</p>
+        <button className='btn'>Empezar ahora</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
